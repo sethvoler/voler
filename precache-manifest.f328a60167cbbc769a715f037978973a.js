5eeec9d81acee86074ffc24512479333aeef0d0c
sethvoler <353619470@qq.com>
2020-01-08 17:01:26 +0800

    Updates

---
+++ precache-manifest.f328a60167cbbc769a715f037978973a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f6113a21bfca97dca02a9448937393",
+    "url": "/voler/index.html"
+  },
+  {
+    "revision": "560b8547e1189ed298e3",
+    "url": "/voler/static/css/main.c7367633.chunk.css"
+  },
+  {
+    "revision": "9bf775e5adcee38cd5cf",
+    "url": "/voler/static/js/2.c56e4c03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/voler/static/js/2.c56e4c03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "560b8547e1189ed298e3",
+    "url": "/voler/static/js/main.6776f63b.chunk.js"
+  },
+  {
+    "revision": "3db6ce5a9b315f37debb",
+    "url": "/voler/static/js/runtime-main.b4e6c342.js"
+  },
+  {
+    "revision": "26df16a442ddb0262615eca37440ef2c",
+    "url": "/voler/static/media/bg.26df16a4.jpg"
+  }
+]);
